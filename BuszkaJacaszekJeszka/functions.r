e310bcdb7125b240ca3143cf65414a72dc916568
mabuszka <magda@buszka.eu>
2019-12-09 08:54:37 +0100

    corrected some mistakes in functions

--- BuszkaJacaszekJeszka/functions.r
+++ BuszkaJacaszekJeszka/functions.r
@@ -2,23 +2,24 @@ library(jsonlite)
 library(lubridate)
 library(dplyr)
 library(tidyr)
-##returns dataframe with data from spotify
+##returns dataframe with streaming history data from spotify
 Streaming_History_df <- function(folder_path){
   file_paths<- list.files(folder_path,"StreamingHistory")
   read_files <- lapply(file_paths, jsonlite::fromJSON)
   bind_rows(read_files)
 }
 
+##changes names 
 names_change <- function(streaming_history, column_names = c("end_time", "artist_name", "track_name", "s_played")){
   names(streaming_history) <- column_names
   streaming_history
 }
 
 ##returns streaming history with end time changed to date
-mutate(streaming_history, end_time = ymd_hm(end_time))
+#mutate(streaming_history, end_time = ymd_hm(end_time))
 
 ##returns streaming_history with end_time column changed from character to date
-mutate(streaming_history, s_played = dmilliseconds((s_played)))
+#mutate(streaming_history, s_played = dmilliseconds((s_played)))
 
 ##returns streaming_history with added start_time column using end_time and ms_played
 add_start_time <- function(streaming_history){
