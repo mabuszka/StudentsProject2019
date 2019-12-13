9bb756335e5bf8d3b3bd4367f48e2168f7fde214
mabuszka <magda@buszka.eu>
2019-12-13 14:28:06 +0100

    adds some data visualization functions

---
+++ BuszkaJacaszekJeszka/data_vis_functions.R
@@ -0,0 +1,42 @@
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+##visualizes number of songs played in given time period at different hours 
+number_of_songs_listened_by_hour <- function(streaming_history, start_date, end_date, 
+                                      by_weekday = FALSE, dont_show_skipped = TRUE){
+  filtered <- filter(streaming_history, start_time >= ymd(start_date), start_time <= ymd(end_date)) 
+  if (dont_show_skipped) filtered <- filter(filtered, skipped == FALSE)
+  vis <- ggplot(filtered, aes(x = hour(start_time)))+
+    geom_bar()+
+    scale_x_discrete(limits = 0:24)+
+    xlab("Hour")+
+    ylab("Songs listened")
+    
+  if (by_weekday){
+    vis <- vis+
+      facet_wrap(~weekday)
+  }
+  vis
+}
+
+number_of_songs_listened_by_weekday <- function(streaming_history, start_date, end_date, dont_show_skipped = TRUE){
+  filtered <- filter(streaming_history, start_time >= ymd(start_date), start_time <= ymd(end_date)) 
+  if (dont_show_skipped) filtered <- filter(filtered, skipped == FALSE)
+  vis <- ggplot(filtered, aes(x = weekday))+
+    geom_bar()+
+    xlab("Weekday")+
+    theme(axis.text.x = element_text(angle = 270))+
+    ylab("Songs listened")
+  vis
+}
+
+number_of_skipped_songs <- function(streaming_history, start_date, end_date, by = "day", type = "bar"){
+  filtered <- filter(streaming_history, start_time >= ymd(start_date), start_time <= ymd(end_date), skipped == TRUE) %>% 
+  mutate(end_time = floor_date(end_time, by))
+  vis <- ggplot(filtered, aes(x = end_time ))+
+    ylab("Songs skipped")+
+    xlab("Date")
+  if (type == "point") vis <- vis + geom_point(stat = "count")
+  if (type == "bar") vis <- vis + geom_bar()
+    vis
+  } 
